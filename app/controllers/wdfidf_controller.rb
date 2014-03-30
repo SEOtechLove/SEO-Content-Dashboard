37591532c750e22e012449e81c5dc9478bfc135f
heikoStammel <heiko.stammel@gmail.com>
2014-03-31 00:06:51 +0200

    WDF Berechnung hinzugefügt

--- app/controllers/wdfidf_controller.rb
+++ app/controllers/wdfidf_controller.rb
@@ -1,8 +1,10 @@
 require 'open-uri'
 
+
 class WdfidfController < ApplicationController
   
   def index
+    
   end
  
   
@@ -14,14 +16,11 @@ class WdfidfController < ApplicationController
       content = get_content(url)
       @view << analyze_content(content)
     end
-    serp_amount = get_serp_amount(keyword)
-    render :index  
+    render :index
     
   end 
   
- 
-  
-  
+
   private
   
   def analyze_content(content)
@@ -29,16 +28,44 @@ class WdfidfController < ApplicationController
     # Rückgabe (term,anzahl)
     term = content[:result_text]
     term_counts = term.group_by{|i| i}.map{|k,v| [k, v.count] } 
+    term_counts_min = filter_terms(term_counts, 3)
 
+    # Wdf pro Term Berechnen
+    # Rückgabe (term,wdf)
+    count = content[:count]
+    wdf_term = term_counts_min.map {|k, v| get_wdf(k, v, count) }
+    
+    # IDF pro Term Berechnen
+    
+    
     return{
-      :url_host => content[:url_host],
-      :url => content[:url],
-      :title => content[:title],
-      :title_count => content[:title_count],
-      :description => content[:description], 
-      :description_count => content[:description_count],
-      :term_count_in_text_and_amount => term_counts,
-      :count => content[:count]
+     # :url_host => content[:url_host],
+       :url => content[:url],
+       :title => content[:title],
+    #  :title_count => content[:title_count],
+       :description => content[:description], 
+    #  :description_count => content[:description_count],
+       :term_count_filtered => term_counts_min,
+       :wdf_term => wdf_term,
+       :count=> content[:count]
+    }
+  end
+  
+  def filter_terms(term_counts, min_amount)
+    #Filtert alle Terme, die weniger als min_amount vorkommen
+   term_counts_min = term_counts.delete_if{|k,v| v < min_amount}
+   return term_counts_min
+  end
+  
+  def get_number_term_in_urls(keyword)
+    #Zählt die URLs, die das Keyword enthalten (ni)
+    
+    
+    
+    
+    return{
+      :count_urls => count_urls 
+      
     }
   end
   
@@ -54,29 +81,28 @@ class WdfidfController < ApplicationController
     doc = Nokogiri::HTML(open(url ,"User-Agent" => "Ruby/#{RUBY_VERSION}"))
     uri = URI("#{url}")
     url_host = uri.host
+   
     # Javascript entfernen
     doc.css('script').remove
     doc.xpath("//@*[starts-with(name(),'on')]").remove
    
-    #Auslesen Meta-Tags und Länge
+    #Auslesen der Meta-Tags und Anzahl der Zeichen inklusive Leerzeichen
     title = get_title(doc)
     title_count = title.length
+    
     description = get_description(doc) 
     description_count = description.length
    
-    #Auslesen des body-Tags und umwandlung in kleinbuchstaben
+    #Auslesen des body-Tags und Umwandlung in Kleinbuchstaben
     html = doc.at('body').inner_text.downcase + title.downcase + description.downcase
    
-    # String mit nur Termen aus Buchstaben  
-    #text  = html.scan(/\p{alnum}[a-zA-Z-]+/)  
-    # String mit allen Termen
-    #text = html.scan(/\p{Alnum}+/)
+    # String mit allen gefundenen Termen  
     text = html.scan(/\p{alpha}+|\d+(?:[\.\-\/]\d+)*/) 
-    #http://solariz.de/de/downloads/6/german_enhanced_stopwords.htm
-    #Bereinigte Wortliste
+    
+    #Terme mit Hilfe von Stopwort-Liste bereinigen (Stopwords-Liste: #http://solariz.de/de/downloads/6/german_enhanced_stopwords.htm + eigene)
     result_text = text - config.STOPWORDS
     
-    #Anzahl Wörter
+    #Anzahl Terme innerhalb eines Dokumentes
     count = result_text.count
     
     return {
@@ -113,20 +139,24 @@ class WdfidfController < ApplicationController
     results_number = doc.xpath('//div[@id="resultStats"]').text
     results_number.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     serp_amount = results_number.scan(/\d+/).join("").to_i
-    return :serp_amount
+   # serp_amount_keyword = {serp_amount
+    return :serp_amount => serp_amount# :serp_amount_keyword => serp_amount_keyword 
   end
   
-  def get_wdf(url, keyword, freq, l)
-     wdf = log2(freq + 1) /  log2(l)
-     return wdf
+  def get_wdf(keyword, freq, l)
+     wdf = ((Math.log((freq + 1), 2)) /  (Math.log(l,2)))
+     wdf = wdf.round(4)
+     wdf_keyword = keyword
+     return {
+      :wdf_keyword => wdf_keyword, 
+      :wdf => wdf}
   end
   
-  def get_idf(url, keyword, nd, ni)
-     idf = log10(1 + nd / ni)
-     return idf
+  def get_idf(keyword, nd, ni)
+     #ni = 
+     nd = content[:serp_amount]
+     idf = Math.log(1 + nd / ni, 10)
+     return {keyword => keyword, idf => idf}
   end
   
-  def get_wdf_idf(wdf , idf)
-     return wdf*idf
-  end
 end
